5d2ac565a98d8533f95bb4dc035238de88ae59b8
aycabta <aycabta@gmail.com>
2019-10-09 18:26:15 +0900

    Support JRuby

--- lib/readline.rb
+++ lib/readline.rb
@@ -1,6 +1,17 @@
-begin
-  require 'readline.so'
-rescue LoadError
-  require 'reline'
+if defined? JRUBY_VERSION
+  begin
+    gem 'reline'
+    require 'reline'
+  rescue LoadError
+    $stderr << "You must install the `reline` gem to use readline"
+  end
+
   Readline = Reline
+else
+  begin
+    require 'readline.so'
+  rescue LoadError
+    require 'reline'
+    Readline = Reline
+  end
 end
