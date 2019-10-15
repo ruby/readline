283f1004e96506eac59912bf71fe57927bd48101
aycabta <aycabta@gmail.com>
2019-10-15 16:42:09 +0900

    Revert "Support JRuby"
    This reverts commit 5d2ac565a98d8533f95bb4dc035238de88ae59b8.

--- lib/readline.rb
+++ lib/readline.rb
@@ -1,17 +1,6 @@
-if defined? JRUBY_VERSION
-  begin
-    gem 'reline'
-    require 'reline'
-  rescue LoadError
-    $stderr << "You must install the `reline` gem to use readline"
-  end
-
+begin
+  require 'readline.so'
+rescue LoadError
+  require 'reline'
   Readline = Reline
-else
-  begin
-    require 'readline.so'
-  rescue LoadError
-    require 'reline'
-    Readline = Reline
-  end
 end
